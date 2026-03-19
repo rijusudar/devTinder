96ad94a0b2c89d8ccd566f1b2ea8c33a850ee406
Riju Sudar <rijusudar@Rijus-MacBook-Pro.local>
2026-03-19 14:57:23 +0530

    Explore all http methods

--- src/app.js
+++ src/app.js
@@ -4,17 +4,38 @@ const app = express();
 
 const port = 3000;
 
-app.use('/', (req, res) => {
-    res.send('Hello World');
+
+app.get('/user', (req, res) => {
+    res.send({
+        firstName: 'Riju',
+        lastName: 'Sudar'
+    })
 });
 
-app.use('/helo', (req, res) => {
-    res.send('Hello Helo helofrom the server');
+app.post('/user', (req, res) => {
+    console.log('Creating a new user...');
+    res.send('User created successfully');
+});
+
+app.delete('/user', (req, res) => {
+    console.log('Deleting a user...');
+    res.send('User deleted successfully');
+});
+
+app.put('/user', (req, res) => {
+    console.log('Updating a user...');
+    res.send('User updated successfully');
+});
+
+app.patch('/user', (req, res) => {
+    console.log('Partially updating a user...');
+    res.send('User partially updated successfully');
 });
 
 app.use('/test', (req, res) => {
     res.send('Hello from the server');
 });
+
 
 app.listen(port, () => {
     console.log(`Server is succesfully on port ${port}...`);
